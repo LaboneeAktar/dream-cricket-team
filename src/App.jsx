78fefbc14b852fbe17a310f3e1c3a7a8ea20548b
LaboneeAktar <labonipub@gmail.com>
2025-07-19 20:08:40 +0600

    Set google font

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ function App() {
       <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
         Button
       </button>
+
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam labore ut
+        officiis voluptas hic, iure placeat maiores nesciunt minima fugit!
+        Provident, maxime quod magni fuga error et facere veniam soluta.
+      </p>
     </>
   );
 }
