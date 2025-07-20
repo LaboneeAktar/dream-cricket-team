ff3fc1363143ebc1b6ab0a090b5cd969de1fbf59
LaboneeAktar <labonipub@gmail.com>
2025-07-20 17:20:33 +0600

    Created a Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-        Button
-      </button>
-
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam labore ut
-        officiis voluptas hic, iure placeat maiores nesciunt minima fugit!
-        Provident, maxime quod magni fuga error et facere veniam soluta.
-      </p>
+      <Home></Home>
     </>
   );
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../../Components/Navbar/Nav";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav></Nav>
+      <h2>Hello home</h2>
+    </div>
+  );
+};
+
+export default Home;
