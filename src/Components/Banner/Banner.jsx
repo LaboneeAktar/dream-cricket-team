448dbee31a6f25da3db561b3e7267af7216e81c7
LaboneeAktar <labonipub@gmail.com>
2025-07-20 20:34:44 +0600

    Completed banner section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,23 +1,33 @@
-import bannerImage from "../../assets/images/bg-shadow.png";
+import bannerShadow from "../../assets/images/bg-shadow.png";
+import bannerImage from "../../assets/images/banner-main.png";
 const Banner = () => {
   return (
     <div>
       <div
-        className="hero w-[1300px] h-[445px] mx-auto rounded-4xl bg-black"
+        className="hero lg:w-[1300px] lg:h-[490px] mx-auto rounded-2xl bg-slate-900"
         style={{
-          backgroundImage: `url(${bannerImage})`,
+          backgroundImage: `url(${bannerShadow})`,
         }}
       >
-        <div className=""></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+        <div className="hero-content text-neutral-content">
+          <div className="">
+            <img
+              className="mx-auto h-48 w-56 object-cover mb-5 lg:mb-6"
+              src={bannerImage}
+              alt=""
+            />
+
+            <h1 className="mb-5 text-xl lg:text-3xl font-bold text-center">
+              Assemble Your Ultimate Dream 11 Cricket Team
+            </h1>
+            <div className="max-w-md text-center mx-auto">
+              <p className="mb-5">Beyond Boundaries Beyond Limits</p>
+              <span className="border border-[#E7FE29] px-1 py-3 rounded-xl">
+                <button className="px-4 py-2 text-sm font-semibold text-black bg-[#E7FE29] border border-lime-400 rounded-xl shadow-md hover:bg-lime-300 hover:shadow-lg transition-all duration-300">
+                  Claim Free Credit
+                </button>
+              </span>
+            </div>
           </div>
         </div>
       </div>
