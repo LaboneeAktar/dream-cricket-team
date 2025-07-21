6e7497ee6b5c006c59018b874d0796d115bb82ac
LaboneeAktar <labonipub@gmail.com>
2025-07-21 11:52:11 +0600

    Designed Player Card

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,24 +1,59 @@
+import user from "../../assets/user.png";
+import { FaFlag } from "react-icons/fa6";
+import { TiStarFullOutline } from "react-icons/ti";
+
 const Player = ({ player }) => {
   const { name, cover, nationality, playing_type, ratings, type, price } =
     player;
   return (
     <div>
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <figure>
+      <div className="card bg-base-100 w-96 shadow-lg">
+        {/* Cover Image */}
+        <figure className="p-5">
           <img
-            className="w-full h-[280px] object-fit rounded-b-md"
+            className="w-full h-[260px] object-fit rounded-lg"
             src={cover}
             alt={`Image of ${name}`}
           />
         </figure>
-        <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+
+        <div className="pt-0 px-5 pb-5">
+          {/* Profile info */}
+          <div className="flex gap-3">
+            <img className="w-6 h-6" src={user} alt="User" />
+            <h2 className="text-lg font-bold">{name}</h2>
+          </div>
+
+          <div className="flex justify-between items-center py-3">
+            <span className="flex gap-1 items-center">
+              <FaFlag className="text-gray-500" />
+              <p className="text-sm text-gray-500">{nationality}</p>
+            </span>
+            <p className="px-4 py-2 bg-base-300 rounded-lg text-sm">
+              {playing_type}
+            </p>
+          </div>
+          <hr className="text-gray-300" />
+
+          {/* Player Details */}
+          <div className="text-sm font-semibold space-y-4 pt-3">
+            <span className="flex justify-between">
+              <p>Rating</p>
+              <span className="flex items-center justify-center gap-1">
+                <TiStarFullOutline className="text-lg text-yellow-400" />{" "}
+                <p className="text-gray-500">{ratings}</p>
+              </span>
+            </span>
+            <span className="flex justify-between">
+              <p>Type</p>
+              <p className="text-gray-500">{type}</p>
+            </span>
+            <span className="flex justify-between items-center">
+              <p>Price : {price}</p>
+              <button className=" text-sm py-1 px-3 border border-slate-300 bg-base-300 rounded-md text-gray-800 cursor-pointer hover:bg-[#E7FE29] hover:shadow-lg transition-all duration-100">
+                Choose Player
+              </button>
+            </span>
           </div>
         </div>
       </div>
--- src/Components/Players/Players.jsx
+++ src/Components/Players/Players.jsx
@@ -13,16 +13,16 @@ const Players = () => {
   }, []);
 
   return (
-    <div className="mx-auto">
-      <div className="flex justify-between mx-10 mt-10 lg:mt-20">
-        <h2 className="text-2xl font-bold">
+    <div className="lg:mx-auto">
+      <div className="flex justify-between mx-3 lg:mx-10 mt-10 lg:mt-20">
+        <h2 className="text-lg lg:text-2xl font-bold">
           Available Players : {players.length}
         </h2>
 
         <div className="flex rounded-lg overflow-hidden border border-gray-300 w-fit">
           <button
             onClick={() => setActiveTab("available")}
-            className={`px-5 py-2 text-sm font-semibold transition-all duration-200 ${
+            className={`px-5 py-2 text-sm font-semibold transition-all duration-200 cursor-pointer ${
               activeTab === "available"
                 ? "bg-[#E7FE29] text-black"
                 : "bg-white text-gray-400"
@@ -32,7 +32,7 @@ const Players = () => {
           </button>
           <button
             onClick={() => setActiveTab("selected")}
-            className={`px-5 py-2 text-sm font-semibold transition-all duration-200 ${
+            className={`px-5 py-2 text-sm font-semibold transition-all duration-200 cursor-pointer ${
               activeTab === "selected"
                 ? "bg-[#E7FE29] text-black"
                 : "bg-white text-gray-400"
@@ -43,7 +43,7 @@ const Players = () => {
         </div>
       </div>
 
-      <div className="mt-10 mx-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 items-center">
+      <div className="mt-10 lg:mx-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 items-center">
         {players.map((player) => (
           <Player key={player.id} player={player}></Player>
         ))}
