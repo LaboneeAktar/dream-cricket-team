ab2eec87b255b388c91f94c45f8720224a87d554
LaboneeAktar <labonipub@gmail.com>
2025-07-21 10:38:37 +0600

    Update json data

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-
-const Player = () => {
+const Player = ({ player }) => {
+  const { name, cover, nationality, playing_type, ratings, type, price } =
+    player;
   return (
     <div>
       <div className="card bg-base-100 w-96 shadow-sm">
         <figure>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
+            className="w-full h-[280px] object-fit rounded-b-md"
+            src={cover}
+            alt={`Image of ${name}`}
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Card Title</h2>
+          <h2 className="card-title">{name}</h2>
           <p>
             A card component has a figure, a body part, and inside body there
             are title and actions parts
